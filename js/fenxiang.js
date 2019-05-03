2a2f7f36e77b7d9d8db192add4a064cfb7725796
jc； <1942655252@qq.com>
2019-05-03 14:48:30 +0800

    消失

---
+++ js/fenxiang.js
@@ -0,0 +1,8 @@
+$(function(){
+	$('#fenxiang').click(function(){
+		$('.fenxiang').slideDown(200);
+	})
+	$('.quxiao').click(function(){
+		$('.fenxiang').slideUp(200);
+	})
+})
---
+++ js/xiaoshi.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+	var er = document.getElementById('er');
+	var to = document.getElementById('to');
+	to.onclick = function() {
+		er.style.display = 'none';
+	}
+}
